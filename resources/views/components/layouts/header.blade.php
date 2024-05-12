e0c4c85fd529f1cbc49c32f0c0325e2b9178073b
karintomania <19652340+karintomania@users.noreply.github.com>
2024-05-12 18:27:00 +0100

    add icon to header

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,8 +1,9 @@
 <header class="bg-base z-20 relative sm:fixed w-full">
-    <div class="hidden sm:flex justify-between px-10 py-4 shadow-lg shadow-cyan-500/20">
-        <div><a href="/" class="
-                bg-main px-4 py-2 font-bold text-2xl transition-colors duration-500 text-transparent bg-clip-text hover:bg-gradient-to-r hover:from-main hover:to-secondary
-            ">VisaHack</a></div>
+    <div class="hidden sm:flex items-center justify-between px-10 py-4 shadow-lg shadow-cyan-500/20">
+        <a href="/" class="flex items-center gap-2">
+            <div class="font-bold text-2xl text-grad">VisaHack</div>
+            <img class="block h-10" src="{{url('/images/home/icon.svg')}}" />
+        </a>
         <div class="text-main text-xl flex flex-col sm:flex-row gap-2 sm:gap-10">
             <a class="hover:text-secondary" href="{{url('/search')}}">Jobs</a>
             <a class="duration-500 hover:text-secondary" href="{{url('/blogs')}}">Blogs</a>
@@ -10,7 +11,10 @@
     </div>
     <div class="flex sm:hidden flex-col px-5 py-2 shadow-lg shadow-cyan-500/20">
         <div class="flex justify-between">
-            <div><a href="{{url('/')}}" class="text-main px-2 py-1 text-2xl font-bold">VisaHack</a></div>
+            <a href="/" class="flex items-center gap-2">
+                <div class="font-bold text-2xl text-grad">VisaHack</div>
+                <img class="block h-10" src="{{url('/images/home/icon.svg')}}" />
+            </a>
             <div id="show-nav-list" class="cursor-pointer text-xl text-main font-bold" onClick="header.toggleNavList()">...</div>
             <div id="hide-nav-list" class="hidden cursor-pointer text-main text-xl font-bold" onClick="header.toggleNavList()">X</div>
         </div>
