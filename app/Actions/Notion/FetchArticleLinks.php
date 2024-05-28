e5040e3e8c5c65ad315d791768dfd69d8ef71909
karintomania <19652340+karintomania@users.noreply.github.com>
2024-05-28 10:13:51 +0100

    add caching of slugs in FetchArticleLinks Action

--- app/Actions/Notion/FetchArticleLinks.php
+++ app/Actions/Notion/FetchArticleLinks.php
@@ -3,20 +3,23 @@
 namespace App\Actions\Notion;
 
 use App\Models\Notion\ArticleLink;
+use App\Models\Notion\Slug;
 use App\Repository\Notion\ArticleLinkCache;
-use Illuminate\Http\Client\Response;
+use App\Repository\Notion\ArticleSlugCache;
 
 class FetchArticleLinks
 {
     public function __construct(
         private CallDatabaseQuery $cdq,
-        private ArticleLinkCache $articleLinkCache,
-    ){}
+        private ArticleLinkCache $linkCache,
+        private ArticleSlugCache $slugCache,
+    ) {
+    }
 
     public function __invoke(): array
     {
-        $json = $this->articleLinkCache->has()
-            ? $this->articleLinkCache->get()
+        $json = $this->linkCache->has()
+            ? $this->linkCache->get()
             : $this->getLinksAndCache();
 
         $jsonData = json_decode($json, true);
@@ -28,18 +31,32 @@ class FetchArticleLinks
         }
 
         return $links;
-        
-    }
 
+    }
 
     private function getLinksAndCache(): string
     {
-         $json = $this->cdq->__invoke();
+        $json = $this->cdq->__invoke();
 
-         // cache $json 
-         $this->articleLinkCache->store($json);
+        $this->cacheLinkAndSlug($json);
 
-         return $json;
+        return $json;
     }
-    
+
+    private function cacheLinkAndSlug(string $json): void
+    {
+        // cache $json
+        $this->linkCache->store($json);
+
+        $jsonData = json_decode($json, true);
+
+        foreach ($jsonData['results'] as $articleData) {
+            $slug = Slug::fromJsonArray($articleData);
+
+            if (! $this->slugCache->has($slug->slug)) {
+                $this->slugCache->store($slug);
+            }
+        }
+
+    }
 }
--- app/Repository/Notion/ArticleSlugCache.php
+++ app/Repository/Notion/ArticleSlugCache.php
@@ -2,18 +2,20 @@
 
 namespace App\Repository\Notion;
 
-use Illuminate\Support\Facades\Cache;
 use App\Models\Notion\Slug;
+use Illuminate\Support\Facades\Cache;
 
-class ArticleSlugCache{
+class ArticleSlugCache
+{
+    public const KEY = 'notion_article_slug_%s';
 
-    public const KEY = "notion_article_slug_%d";
     public const LIFETIME_SECONDS = 12 * 60 * 60; // 12 hours
 
     public function has(string $slugStr): bool
     {
         $key = $this->getKey($slugStr);
         $has = Cache::has($key);
+
         return $has;
     }
 
@@ -21,22 +23,18 @@ class ArticleSlugCache{
     {
         $key = $this->getKey($slugStr);
         $slug = Cache::get($key);
+
         return $slug;
     }
 
-    /**
-     * @return void
-     */
     public function store(Slug $slug): void
     {
         $key = $this->getKey($slug->slug);
         Cache::put($key, $slug, self::LIFETIME_SECONDS);
-
     }
 
     private function getKey(string $slugStr): string
     {
         return sprintf(self::KEY, $slugStr);
     }
-    
 }
