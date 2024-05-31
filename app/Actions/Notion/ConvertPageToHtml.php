dcee1c19253d4c225c420de1303e91af892231d5
karintomania <19652340+karintomania@users.noreply.github.com>
2024-05-31 22:49:55 +0100

    add ConvertPageToHtml

--- app/Actions/Notion/ConvertPageToHtml.php
+++ app/Actions/Notion/ConvertPageToHtml.php
@@ -9,23 +9,60 @@ class ConvertPageToHtml
 
         $data = json_decode($json, true)['results'];
         $html = '';
+        $openingList = '';
 
         foreach ($data as $block) {
             $type = $block['type'];
 
-            $content = match ($type) {
-                'heading_1' => $this->handleHeading1($block),
-                'heading_2' => $this->handleHeading2($block),
-                'heading_3' => $this->handleHeading3($block),
-                'paragraph' => $this->handleParagraph($block),
-                'image' => $this->handleImage($block),
-                default => 'defalut'
-
-            };
-            $html .= $content."\n";
+            if (($type === 'numbered_list_item' || $type === 'bulleted_list_item')) {
+                [$html, $openingList] = $this->handleListItem($openingList, $type, $html, $block);
+            } else {
+                [$html, $openingList] = $this->handleBlock($openingList, $type, $html, $block);
+            }
         }
 
         return $html;
+    }
+
+    private function handleListItem(string $openingList, string $type, string $html, array $block): array
+    {
+
+        $listType = $type === 'numbered_list_item' ? 'ol' : 'ul';
+
+        if (! $openingList) {
+            // open a new list here
+            $openingList = $listType;
+            $html .= "<{$openingList}>\n";
+        }
+
+        $content = $type === 'numbered_list_item'
+            ? $this->handleNumberedListItem($block)
+            : $this->handleBulletedListItem($block);
+
+        $html .= $content . "\n";
+
+        return [$html, $openingList];
+    }
+
+    private function handleBlock(string $openingList, string $type, string $html, array $block)
+    {
+        if ($openingList) {
+            // if list is open, close here
+            $html .= "</{$openingList}>\n";
+            $openingList = '';
+        }
+
+        $content = match ($type) {
+            'heading_1' => $this->handleHeading1($block),
+            'heading_2' => $this->handleHeading2($block),
+            'heading_3' => $this->handleHeading3($block),
+            'paragraph' => $this->handleParagraph($block),
+            'image' => $this->handleImage($block),
+            default => 'defalut'
+        };
+        $html .= $content . "\n";
+
+        return [$html, $openingList];
     }
 
     private function handleImage(array $block): string
@@ -67,26 +104,47 @@ class ConvertPageToHtml
 
                 $annotations = $text['annotations'];
 
-                if ($annotations['bold']) {
-                    $plain = "<b>{$plain}</b>";
-                }
-                if ($annotations['italic']) {
-                    $plain = "<i>{$plain}</i>";
-                }
-                if ($annotations['strikethrough']) {
-                    $plain = "<s>{$plain}</s>";
-                }
-                if ($annotations['underline']) {
-                    $plain = "<u>{$plain}</u>";
-                }
-                if ($annotations['code']) {
-                    $plain = "<code>{$plain}</code>";
+                if ($link = data_get($text, 'text.link')) {
+                    $url = $link['url'];
+
+                    return $carry."<a href=\"{$url}\">{$plain}</a>";
+                } else {
+                    if ($annotations['bold']) {
+                        $plain = "<b>{$plain}</b>";
+                    }
+                    if ($annotations['italic']) {
+                        $plain = "<i>{$plain}</i>";
+                    }
+                    if ($annotations['strikethrough']) {
+                        $plain = "<s>{$plain}</s>";
+                    }
+                    if ($annotations['underline']) {
+                        $plain = "<u>{$plain}</u>";
+                    }
+                    if ($annotations['code']) {
+                        $plain = "<code>{$plain}</code>";
+                    }
+
+                    return $carry.$plain;
                 }
 
-                return $carry .= $plain;
             }
         );
 
         return "<p>{$content}</p>";
+    }
+
+    private function handleBulletedListItem(array $block): string
+    {
+        $content = data_get($block, 'bulleted_list_item.text.0.plain_text');
+
+        return "<li>{$content}</li>";
+    }
+
+    private function handleNumberedListItem(array $block): string
+    {
+        $content = data_get($block, 'numbered_list_item.text.0.plain_text');
+
+        return "<li>{$content}</li>";
     }
 }
--- tests/Feature/Actions/Notion/ConvertPageToHtmlTest.php
+++ tests/Feature/Actions/Notion/ConvertPageToHtmlTest.php
@@ -5,19 +5,29 @@ namespace Tests\Feature\Actions\Notion;
 use App\Actions\Notion\ConvertPageToHtml;
 use Tests\TestCase;
 
-class ConvertPageToHtmlTest extends TestCase
-{
+class ConvertPageToHtmlTest extends TestCase{
+
     public function test_convert_converts(): void
     {
 
-        $expected = <<<'HTML'
-        <h1>Test Blog</h1>
+        $expected = <<<HTML
+        <h1>How to write a test Blog using Notion</h1>
         <p>This is a test article. Let’s try not to use blocks for multiple lines.
         I can use Shift + Enter for that.</p>
         <h2>This is a header 2</h2>
         <h3>This is a header 3</h3>
         <p>This is with <u><b>style</b></u>.</p>
-        <img src="http://example.com/picture.gif">
+        <p>This is with link: <a href="https://example.com">link to somewhere</a>.</p>
+        <ol>
+        <li>numbered 1</li>
+        <li>numbered 2</li>
+        </ol>
+        <p></p>
+        <ul>
+        <li>bulleted 1</li>
+        <li>bulleted 2</li>
+        </ul>
+        <img src="https://fastly.picsum.photos/id/686/800/800.jpg?hmac=YqfsJsRmmHGlApJA9jf_TKx1v4U2N4FPoBzj9rdqilo">
 
         HTML;
         $converter = new ConvertPageToHtml();
@@ -28,4 +38,7 @@ class ConvertPageToHtmlTest extends TestCase
 
         $this->assertEquals($expected, $result);
     }
+
+    
+    
 }
