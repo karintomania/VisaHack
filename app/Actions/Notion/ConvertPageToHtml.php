7277499e02f0e1c779808027e6a34fab7cb461d4
karintomania <19652340+karintomania@users.noreply.github.com>
2024-05-31 16:13:02 +0100

    add ConvertPageToHtml

---
+++ app/Actions/Notion/ConvertPageToHtml.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Actions\Notion;
+
+
+class ConvertPageToHtml {
+    
+    public function convert(string $json): string
+    {
+
+        $data = json_decode($json, true)['results'];
+        $html = "";
+
+        foreach($data as $block) {
+            $type = $block['type'];
+
+            $content = match ($type) {
+                "heading_1" => $this->handleHeading1($block),
+                "heading_2" => $this->handleHeading2($block),
+                "heading_3" => $this->handleHeading3($block),
+                "paragraph" => $this->handleParagraph($block),
+                "image" => $this->handleImage($block),
+                default => "defalut"
+
+            };
+            $html .= $content . "\n";
+        }
+
+        return $html;
+    }
+
+    private function handleImage(array $block): string
+    {
+        $src = data_get($block, 'image.file.url');
+        return "<img src=\"{$src}\">";
+    }
+
+    private function handleHeading1(array $block): string
+    {
+        
+        $content = data_get($block, 'heading_1.text.0.plain_text');
+        return "<h1>{$content}</h1>";
+    }
+
+    private function handleHeading2(array $block): string
+    {
+        $content = data_get($block, 'heading_2.text.0.plain_text');
+        return "<h2>{$content}</h2>";
+    }
+
+    private function handleHeading3(array $block): string
+    {
+        $content = data_get($block, 'heading_3.text.0.plain_text');
+        return "<h3>{$content}</h3>";
+    }
+
+    private function handleParagraph(array $data): string
+    {
+
+        $content = array_reduce(
+            data_get($data, 'paragraph.text'),
+            function ($carry, $text) {
+                $plain = $text['plain_text'];
+
+                $annotations = $text['annotations'];
+
+                if($annotations['bold']) $plain = "<b>{$plain}</b>";
+                if($annotations['italic']) $plain = "<i>{$plain}</i>";
+                if($annotations['strikethrough']) $plain = "<s>{$plain}</s>";
+                if($annotations['underline']) $plain = "<u>{$plain}</u>";
+                if($annotations['code']) $plain = "<code>{$plain}</code>";
+
+                return $carry .= $plain;
+            }
+        );
+
+        return "<p>{$content}</p>";
+    }
+    
+}
---
+++ tests/Feature/Actions/Notion/ConvertPageToHtmlTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Actions\Notion;
+
+use App\Actions\Notion\ConvertPageToHtml;
+use Tests\TestCase;
+
+class ConvertPageToHtmlTest extends TestCase{
+
+    public function test_convert_converts(): void
+    {
+
+        $expected = <<<HTML
+        <h1>Test Blog</h1>
+        <p>This is a test article. Let’s try not to use blocks for multiple lines.
+        I can use Shift + Enter for that.</p>
+        <h2>This is a header 2</h2>
+        <h3>This is a header 3</h3>
+        <p>This is with <u><b>style</b></u>.</p>
+        <img src="http://example.com/picture.gif">
+
+        HTML;
+        $converter = new ConvertPageToHtml();
+
+        $json = file_get_contents(dirname(__FILE__).'/convert_page_test.json');
+
+        $result = $converter->convert($json);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    
+    
+}
