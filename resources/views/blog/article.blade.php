e5b3193462c1a4e08c3041b88298c56fcbb16f7b
karintomania <19652340+karintomania@users.noreply.github.com>
2024-06-10 20:30:40 +0100

    improve css

--- resources/views/blog/article.blade.php
+++ resources/views/blog/article.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.base>
     <section class="w-full flex flex-col items-center px-4 mb-10">
-        <div class="flex flex-col items-start w-full sm:w-4/5 xl:w-2/3 bg-base-light px-4 py-10 border border-gray-700 blog-article">
+        <div class="flex flex-col items-start w-full sm:w-4/5 xl:w-2/3 bg-base-light px-4 sm:px-16 py-10 border border-gray-700 blog-article">
         {!! $html !!}
         </div>
     </section>
