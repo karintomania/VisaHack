45d7a921cd2a3fe84873cf61bb5d44cd5146fe67
karintomania <19652340+karintomania@users.noreply.github.com>
2024-08-13 16:54:55 +0100

    remove hot file on deployment

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,15 @@ if [[ -z $1 ]]; then
   exit 1
 fi
 
-APP_RELEASE_PATH="$1"
-APP_RELEASE_PATH=$(echo $APP_RELEASE_PATH | sed 's/visahack_//' | sed 's/.tar.gz//')
+TAR_PATH="$1"
+APP_RELEASE_PATH=$(echo $TAR_PATH | sed 's/visahack_//' | sed 's/.tar.gz//')
 
-tar xzf "$1"
+tar xzf "$TAR_PATH"
 chmod -R 775 app
 sudo chown -R www-data:www-data app
 mv app $APP_RELEASE_PATH
+
+# remove hot file
+rm $APP_RELEASE_PATH/public/hot
 
 ln -sfn /var/www/visahack-releases/${APP_RELEASE_PATH} /var/www/visahack
